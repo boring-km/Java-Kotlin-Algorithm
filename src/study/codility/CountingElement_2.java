a4bd7027c34ce0cea7d4e14cb1a53aaf0c7a0e05
boring-km <kms0644804@naver.com>
2021-02-18 22:42:42 +0900

    [master] max counter 나중으로 미룸

--- src/study/codility/CountingElement_2.java
+++ src/study/codility/CountingElement_2.java
@@ -11,15 +11,27 @@ public class CountingElement_2 {
         // write your code in Java SE 8
 
         int[] board = new int[N];
-        int maxi = 0;
+        int max = 0;
+        int max_record = 0;
 
         for (int index : A) {
-            if (index > N) {    // max counter
-                Arrays.fill(board, maxi);
-            } else {    // increase(X)
+            if (index == N + 1) {    // max counter 하지 않고 현재 최대값 저장
+                max_record = max;
+            } else {                 // increase(X)
                 int target = index - 1;
-                board[target] += 1;
-                maxi = Math.max(maxi, board[target]);
+                if (max_record >= board[target]) {
+                    board[target] = max_record + 1;     // max 값을 반영은 해야하니까
+                } else {
+                    board[target] += 1;
+                }
+                max = Math.max(max, board[target]);
+            }
+        }
+
+        // max count를 한번만 세팅, 이미 max가 추가가 된 인덱스에는 추지 않음
+        for (int i = 0; i < board.length; i++) {
+            if (max_record > 0 && max_record > board[i]) {
+                board[i] = max_record;
             }
         }
 
