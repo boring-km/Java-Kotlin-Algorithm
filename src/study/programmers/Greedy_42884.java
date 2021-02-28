e7166132570f8538bd3b2e9bfc7a265df606e630
boring-km <kms0644804@naver.com>
2021-02-28 13:18:17 +0900

    [master] 그리디 문제
    차량이 들어온 지점이 기존 카메라 위치보다 우측에 있으면, 차량이 나가는 지점에 카메라를 설치한다.

---
+++ src/study/programmers/Greedy_42884.java
@@ -0,0 +1,25 @@
+package study.programmers;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+// 1시간 10분 초과.. 포기
+public class Greedy_42884 {
+    public static void main(String[] args) {
+        System.out.println(solution(new int[][]{{-20, 15}, {-14, -5}, {-18, -13}, {-5, -3}}));
+    }
+
+    public static int solution(int[][] routes) {
+        int answer = 0; // 카메라의 갯수
+        int camera = -30001; // 카메라의 위치
+        Arrays.sort(routes, Comparator.comparingInt(a -> a[1]));
+        for (int[] route : routes) {
+            if (camera < route[0]) {
+                camera = route[1];
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+}
