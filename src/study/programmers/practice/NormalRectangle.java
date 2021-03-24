1d0a00b89cde044a00a4d1bcdee8c6847766eb9e
boring-km <kms0644804@naver.com>
2021-03-24 23:50:12 +0900

    [master] 프로그래머스 멀쩡한 사각형 문제 - 다른 사람 풀이
    너무 간결한 풀이 방법으로 대각선에 걸치는 블럭을 제외한다.
    풀이: https://velog.io/@ajufresh/프로그래머스-멀쩡한-사각형-문제풀이-Java
    문제: https://programmers.co.kr/learn/courses/30/lessons/62048

--- src/study/programmers/practice/NormalRectangle.java
+++ src/study/programmers/practice/NormalRectangle.java
@@ -1,21 +1,14 @@
 package study.programmers.practice;
 
+import java.math.BigInteger;
+
 public class NormalRectangle {
     public static void main(String[] args) {
         System.out.println(solution(8, 12));
     }
 
     public static long solution(int w, int h) {
-        long answer = 1;
-        double slope = (double)h / (double) w;
-        for (int y = 1; y < h; y++) {
-            for (int x = 1; x < w; x++) {
-                if (slope > (double) y/ (double) x && slope < (double) y+1 / (double) x) {
-                    System.out.println(y + " " + x);
-                    answer++;
-                }
-            }
-        }
-        return answer;
+        int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();
+        return ((long) w * (long) h) - ((((long) w / gcd) + ((long) h / gcd) - 1) * gcd);
     }
 }
