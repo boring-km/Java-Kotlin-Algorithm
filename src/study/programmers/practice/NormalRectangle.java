dd34066a4c3dd32493ae64d6e0dd0b167ba891e0
boring-km <kms0644804@naver.com>
2021-03-24 23:58:00 +0900

    [master] 다른 사람 풀이를 내 방식으로 정리해보자

--- src/study/programmers/practice/NormalRectangle.java
+++ src/study/programmers/practice/NormalRectangle.java
@@ -8,7 +8,9 @@ public class NormalRectangle {
     }
 
     public static long solution(int w, int h) {
-        int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();
-        return ((long) w * (long) h) - ((((long) w / gcd) + ((long) h / gcd) - 1) * gcd);
+        int gcd = BigInteger.valueOf(w).gcd(BigInteger.valueOf(h)).intValue();  // w와 h 중에 더 큰 값 구하기
+        long area = ((long) w * (long) h);
+        long excluded = (long) w + (long) h - gcd;
+        return area - excluded;
     }
 }
