663ee54c3b3dce55588203a515ad31fdda38e846
boring-km <kms0644804@naver.com>
2021-04-03 00:14:36 +0900

    [master] 프로그래머스 - 순위 검색 풀이 실패
    풀이에 주석 추가

--- src/study/programmers/practice/RankSearching.java
+++ src/study/programmers/practice/RankSearching.java
@@ -21,24 +21,26 @@ public class RankSearching {
                 for (int j = 0; j < 4; j++) {
                     if ((i & (1 << j)) > 0) key.append(split[j]);
                 }
+                // key가 map에 없을때만 추가한다.
                 map.computeIfAbsent(key.toString(), s -> new ArrayList<>()).add(score);
             }
         }
 
         // Map 안에 List sorting
         for (Map.Entry<String, List<Integer>> entry : map.entrySet()){
-            entry.getValue().sort(null);
+            entry.getValue().sort(Comparator.reverseOrder());
         }
 
         int[] answer = new int[query.length];
         for(int i = 0; i < query.length; i++) {
+            // 혀를 내두르게 되는 -, and 변환 및 split
             String[] splits = query[i].replaceAll("-", "").replaceAll(" and ", "").split(" ");
             String key = splits[0];
             int score = Integer.parseInt(splits[1]);
             List<Integer> list = map.getOrDefault(key, new ArrayList<>());
             int start = 0;
             int end = list.size();
-            // lower bound 를 구해야 함
+            // 이분 탐색으로 score 보다 작은 수 중 가장 나중 인덱스 찾음
             while (start < end) {
                 int mid = (start + end) / 2;
                 if(list.get(mid) < score) {
@@ -47,7 +49,7 @@ public class RankSearching {
                     end = mid;
                 }
             }
-            // 전체에서 lower bound를 빼면 score와 같거나 큰 경우의 수
+            // 전체에서 score보다 작은 수를 빼면 score보다 같거나 큰 수의 갯수
             answer[i] = list.size() - start;
         }
         return answer;
