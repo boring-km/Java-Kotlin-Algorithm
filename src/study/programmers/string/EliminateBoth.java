31ded667e8118843e23ed4bd114eec3dcd919d72
boring-km <kms0644804@naver.com>
2021-04-14 14:36:49 +0900

    프로그래머스 - 짝지어 제거하기 (시간초과 해결)
    Stack을 활용하여 스택의 가장 위에 있는 값과 동일하면 오히려 그 값을 빼자
    https://programmers.co.kr/learn/courses/30/lessons/12973

--- src/study/programmers/string/EliminateBoth.java
+++ src/study/programmers/string/EliminateBoth.java
@@ -1,37 +1,36 @@
 package study.programmers.string;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 // 결과가 효율성 테스트에서 시간 초과 나옴
 public class EliminateBoth {
     public static void main(String[] args) {
-        System.out.println(solution("cdcd"));
+        System.out.println(solution("baabaa"));
     }
     public static int solution(String s)
     {
-        int answer = 0;
-
-        while (true) {
-            int size = s.length();
-            StringBuilder record = new StringBuilder();
-            char before = ' ';
-            for (int i = 0; i < size; i++) {
-                if (before == s.charAt(i)) {
-                    record = new StringBuilder(record.substring(0, record.length() - 1));
-                    before = ' ';
-                    continue;
-                }
-                before = s.charAt(i);
-                record.append(s.charAt(i));
-            }
-            if (record.toString().length() == 0) {
-                answer = 1;
-                break;
-            }
-            if (record.toString().length() == size) {
-                break;
-            }
-            s = record.toString();
+        int answer;
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0, len = s.length(); i < len; i++) {
+            stack.add(s.charAt(i));
         }
-
+        ArrayList<Character> cur = new ArrayList<>();
+        while (!stack.isEmpty()) {
+            if (cur.size() == 0) {
+                cur.add(stack.pop());
+                continue;
+            }
+            if (cur.get(cur.size()-1) == stack.get(stack.size()-1)) {
+                cur.remove(cur.size()-1);
+                stack.pop();
+                continue;
+            }
+            cur.add(stack.pop());
+        }
+        int size = cur.size();
+        if (size == 0) answer = 1;
+        else answer = 0;
         return answer;
     }
 }
