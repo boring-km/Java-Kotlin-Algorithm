701668cad13d32699835532fabe9e4021e21def8
boring-km <kms0644804@naver.com>
2021-05-05 21:02:49 +0900

    프로그래머스 - 동굴 탐험
    Stack과 Array를 활용하여 Tree 형태로 구현하지 않고 order 경로를 먼저 탐색하는 방법이다.
    https://programmers.co.kr/learn/courses/30/lessons/67260

---
+++ src/study/programmers/stackqueue/CaveAdventure.java
@@ -0,0 +1,59 @@
+package study.programmers.stackqueue;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class CaveAdventure {
+    public static void main(String[] args) {
+        System.out.println(solution(
+                9,
+                new int[][]{{0, 1}, {0, 3}, {0, 7}, {8, 1}, {3, 6}, {1, 2}, {4, 7}, {7, 5}},
+                new int[][]{{8, 5}, {6, 7}, {4, 1}}
+        ));
+    }
+
+    public static boolean solution(int n, int[][] path, int[][] order) {
+        int[] before = new int[n];
+        int[] after = new int[n];
+        boolean[] visited = new boolean[n];
+        visited[0] = true;
+        ArrayList<Integer> data[] = new ArrayList[n];
+
+        for (int i = 0; i < n; i++) {
+            data[i] = new ArrayList<>();
+        }
+        for (int[] fromTo : path) { // 쌍방향 경로 연결
+            data[fromTo[0]].add(fromTo[1]);
+            data[fromTo[1]].add(fromTo[0]);
+        }
+        for (int[] item : order) {
+            before[item[1]] = item[0];  // to -> from
+        }
+
+        // order에 루트 경로를 넣게 되면 모든 경로를 탐색하기 어렵다.
+        if (before[0] != 0) return false;
+
+        Stack<Integer> stack = new Stack<>();
+        stack.addAll(data[0]); // 0과 연결된 경로 모두 stack에 추가
+
+        while (!stack.isEmpty()) {
+            int cur = stack.pop();
+            if (!visited[cur]) {    // 방문한 적이 없는 노드
+                if (!visited[before[cur]]) {    // order에 있는 값이면 before[cur]가 0이 아니다.
+                    after[before[cur]] = cur;   // 이전 노드의 다음 노드로 cur 추가
+                    continue;
+                }
+                visited[cur] = true;    // order에 없으면 방문
+                stack.addAll(data[cur]);    // 연결된 모든 노드 추가
+                stack.add(after[cur]);  // 다음 노드에도 추가
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {  // 모든 방을 방문하지 않았다면 false
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
