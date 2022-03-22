19f8c651f3baad4ebffc5cf1ed961c3b7626f94d
boring-km <kms0644804@naver.com>
2022-03-23 00:25:10 +0900

    Smallest String With A Given Numeric Value 통과 케이스

--- src/study/leetcode/SmallestString.kt
+++ src/study/leetcode/SmallestString.kt
@@ -2,51 +2,28 @@ package study.leetcode
 
 class SmallestString {
     fun getSmallestString(n: Int, k: Int): String {
-
-        var maxCount = k / 26   // 27 / 26 => 1
-        var maxNormalCount = n - maxCount   // 3 - 1 => 2
-        if (maxNormalCount >= 26) {
-            val overCount = maxNormalCount / 26
-            maxCount -= overCount
-            maxNormalCount += overCount
+        val result = StringBuilder()
+        var count = n
+        var value = k
+        while (count != 0) {
+            val max = 26 * (count-1)
+            if (max >= value) {
+                result.append( "a")
+                value--
+            } else {
+                result.append((value - max + 96).toChar())
+                value = max
+            }
+            count--
         }
 
-        val zCount = if (k < maxCount * 26 + maxNormalCount) maxCount - 1 else maxCount
-
-        val normalCount = n - zCount
-        val maxValue = zCount * 26
-
-        if (normalCount == 0) return "z" * n
-
-        val smallestCount = normalCount - 1
-        var second = (k - maxValue) - smallestCount
-        if (zCount == 0) {
-            second = k - smallestCount
-        }
-
-        var result = ""
-
-        if (normalCount == 1) {
-            result += (second + 96).toChar()
-        } else {
-            result += "a" * smallestCount
-            result += (second + 96).toChar()
-        }
-
-        result += "z" * zCount
-         return result
-    }
-
-    private operator fun String.times(n: Int): String {
-        var result = ""
-        for (i in 0 until n) result += this
-        return result
+        return result.toString()
     }
 }
 
 
 fun main() {
-    val result = SmallestString().getSmallestString(23100, 567226)
+    val result = SmallestString().getSmallestString(5, 130)
     println(result)
 }
 
