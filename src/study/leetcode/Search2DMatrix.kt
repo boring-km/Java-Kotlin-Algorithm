58bcc61663924900b54b842e5c03980cc23b6742
boring-km <kms0644804@naver.com>
2022-03-30 22:35:32 +0900

    leetcode: 74. Search a 2D Matrix

---
+++ src/study/leetcode/Search2DMatrix.kt
@@ -0,0 +1,29 @@
+package study.leetcode
+
+object Search2DMatrix {
+    fun searchMatrix(matrix: Array<IntArray>, target: Int): Boolean {
+
+        val n = matrix.size
+        val m = matrix[0].size
+
+        for (i in 0 until n) {
+            for (j in 0 until m) {
+                if (matrix[i][j] == target) {
+                    return true
+                } else if (matrix[i][j] > target) {
+                    return false
+                }
+            }
+        }
+
+
+        return false
+    }
+}
+
+fun main() {
+    val result = Search2DMatrix.searchMatrix(arrayOf(
+        intArrayOf(1,3,5,7)
+    ), 3)
+    println(result)
+}
