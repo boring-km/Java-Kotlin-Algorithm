265317b9a5fa9cc5a2dd9d7c5aedf59951207e66
boring-km <kms0644804@naver.com>
2022-07-11 13:24:44 +0900

    프로그래머스 2개 이하로 다른 비트 시간 해결

--- src/study/programmers/Programmers_77885.kt
+++ src/study/programmers/Programmers_77885.kt
@@ -1,39 +1,35 @@
 package study.programmers
 
-import java.lang.Long
 
-
+// https://school.programmers.co.kr/learn/courses/30/lessons/77885
+// https://velog.io/@courage331/%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4Java-2%EA%B0%9C-%EC%9D%B4%ED%95%98%EB%A1%9C-%EB%8B%A4%EB%A5%B8-%EB%B9%84%ED%8A%B8
 object Programmers_77885 {
     fun solution(numbers: LongArray): LongArray {
         val answer = LongArray(numbers.size)
 
         for (i in numbers.indices) {
-            val num = numbers[i]
-            val toBinaryNumString = Long.toBinaryString(num)
-            var temp = num
+            var bin = java.lang.Long.toBinaryString(numbers[i])
 
-            while (true) {
-                temp++;
-
-                val toBinaryTempString = Long.toBinaryString(temp)
-                val maxSize = Math.max(toBinaryNumString.length, toBinaryTempString.length)
-
-                val binNum = toBinaryNumString.padStart(maxSize, '0')
-                val binTemp = toBinaryTempString.padStart(maxSize, '0')
-                var count = 0
-                for (j in maxSize-1 downTo 0 ) {
-                    if (binNum[j] != binTemp[j]) {
-                        count++
-                    }
-                    if (count > 2) {
-                        break
-                    }
+            if (numbers[i].mod(2) == 0) {
+                answer[i] = numbers[i] + 1  // 짝수면 뒤에가 10 이거나 00 이니까 +1만 하면 됨
+            } else {
+                val last = bin.lastIndexOf("0")     // 마지막 0 찾기
+                val first = bin.indexOf("1", last)  // 마지막 0 바로 다음에 나오는 1
+                if (last == -1) {   // 0이 없으면 -> 11111
+                    numbers[i] = numbers[i].plus(1) // 1을 더하기 -> 100000
+                    bin = java.lang.Long.toBinaryString(numbers[i]) // 더한 숫자를 이진수로 변환
+                    // 101111
+                    bin = bin.substring(0, 2) +
+                                bin.substring(2, bin.length).replace("0", "1")  // 011111 -> 101111 2개 차이
+                } else {
+                    // 0이 있으면
+                    // ...0 -> 1 ... 1 -> 0 ...
+                    bin = bin.substring(0, last) + "1" +
+                            bin.substring(last + 1, first) + "0" +
+                            bin.substring(first + 1, bin.length)
                 }
-                if (count == 1 || count == 2) {
-                    break
-                }
+                answer[i] = java.lang.Long.parseLong(bin, 2)
             }
-            answer[i] = temp
         }
 
         return answer
@@ -41,10 +37,6 @@ object Programmers_77885 {
 }
 
 fun main() {
-
-    println( Long.toBinaryString(0b1100.and(0b1001)))
-
-//    println(7.and(9))
-//    val results = Programmers_77885.solution(longArrayOf(2,7))
-//    results.forEach { println(it) }
+    val results = Programmers_77885.solution(longArrayOf(2,7))
+    results.forEach { println(it) }
 }
