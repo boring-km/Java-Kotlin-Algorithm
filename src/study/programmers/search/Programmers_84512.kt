94b32aaed7c29c08301d5972739867f446483cf7
boring-km <kms0644804@naver.com>
2022-07-13 08:34:59 +0900

    프로그래머스 모음사전 - 다른 사람의 깔끔한 풀이

--- src/study/programmers/search/Programmers_84512.kt
+++ src/study/programmers/search/Programmers_84512.kt
@@ -1,29 +1,23 @@
 package study.programmers.search
 
 private object Programmers_84512 {
+    val arr = arrayOf("A", "E", "I", "O", "U")
+    val result = mutableListOf<String>()
+
     fun solution(word: String): Int {
-        val sMap = mapOf(0 to 'X', 1 to 'A', 2 to 'E', 3 to 'I', 4 to 'O', 5 to 'U')
-        val set = HashSet<String>()
-        for (a in 0 until 6) {
-            for (b in 0 until 6) {
-                for (c in 0 until 6) {
-                    for (d in 0 until 6) {
-                        for (e in 0 until 6) {
-                            set.add(String(charArrayOf(sMap[a]!!, sMap[b]!!, sMap[c]!!, sMap[d]!!, sMap[e]!!)).replace("X", ""))
-                        }
-                    }
-                }
-            }
+        dfs("")
+        result.forEachIndexed { idx, s ->
+            if(s == word) return idx
         }
-        var answer = 0
-        val myList = set.toList().sorted()
-        for (i in myList.indices) {
-            if (myList[i] == word) {
-                answer = i
-                break
-            }
+        return -1
+    }
+
+    fun dfs(str: String) {
+        if(str.length > 5) return
+        result.add(str)
+        for(a in arr) {
+            dfs(str + a)
         }
-        return answer
     }
 }
 
