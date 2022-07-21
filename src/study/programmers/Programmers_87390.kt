f4c52aab4fb700c24a5884d93f4b4704d6206ff3
boring-km <kms0644804@naver.com>
2022-07-21 23:08:27 +0900

    프로그래머스 n^2 배열 자르기 - 통과..

--- src/study/programmers/Programmers_87390.kt
+++ src/study/programmers/Programmers_87390.kt
@@ -2,55 +2,14 @@ package study.programmers
 
 object Programmers_87390 {
     fun solution(n: Int, left: Long, right: Long): IntArray {
-        val answer: IntArray
 
-        val leftMok = left / n
-        val leftNam = left % n
-
-        val dataList = arrayListOf<Long>()
-        for (i in leftNam until n) {
-            var bonus = 0
-            if (i - leftNam + 1 < leftMok) {
-                dataList.add(leftMok + 1)
-            } else {
-                if (leftNam == 0L && i < leftMok) bonus = 1
-                dataList.add(i + bonus + 1)
-            }
+        val list = arrayListOf<Int>()
+        var temp = left
+        while (temp <= right) {
+            list.add((Math.max(temp / n, temp++ % n) + 1).toInt())
         }
 
-        val diff = (right - left + 1).toInt()
-        if (diff <= dataList.size) {
-            answer = IntArray(diff)
-        } else {
-            // 중간에 추가로 들어가는 행이 있다면
-            val rightMok = right / n
-            val rightNam = right % n
-            if (dataList.size + n < diff) {
-                for (i in 0 until rightMok - leftMok - 1) {
-                    val cur = i + leftMok + 1 + 1
-                    for (j in 0 until n) {
-                        if (j < cur) {
-                            dataList.add(cur)
-                        } else {
-                            dataList.add((j + 1).toLong())
-                        }
-                    }
-                }
-            }
-
-            for (i in 0 .. rightNam) {
-                if (i < rightMok + 1) {
-                    dataList.add(rightMok + 1)
-                } else {
-                    dataList.add(i + rightMok)
-                }
-            }
-            answer = IntArray(dataList.size)
-        }
-        for (i in answer.indices) {
-            answer[i] = dataList[i].toInt()
-        }
-        return answer
+        return list.toIntArray()
     }
 }
 
