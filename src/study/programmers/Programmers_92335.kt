8e9574d6c3d79e34ae2d85fbee35d75058f711f6
boring-km <kms0644804@naver.com>
2022-07-23 21:34:13 +0900

    프로그래머스 k진수에서 소수 개수 구하기 - 성공
    - 소수 구하기
    - k진법 변환하기

---
+++ src/study/programmers/Programmers_92335.kt
@@ -0,0 +1,42 @@
+package study.programmers
+
+private object Programmers_92335 {
+    fun solution(n: Int, k: Int): Int {
+        var answer = 0
+        val str = change(n, k)
+
+        val arr = str.split('0')
+        arr.forEach{
+            if (it.isNotEmpty() && isPrimeNumber(it.toLong())) answer++
+        }
+
+        return answer
+    }
+
+    fun change(num: Int, k: Int): String {
+        var result = ""
+        if (num == 0) return "0"
+        var temp = num
+        while (temp > 0) {
+            result = "${temp % k}" + result
+            temp /= k
+        }
+        return result
+    }
+
+    fun isPrimeNumber(it: Long): Boolean {
+        val root = Math.sqrt(it.toDouble()).toInt()
+        if (it == 1L) return false
+        for (i in 2 .. root) {
+            if (it % i == 0L) {
+                return false
+            }
+        }
+        return true
+    }
+}
+
+fun main() {
+    val result = Programmers_92335.solution(110011, 10)
+    println(result)
+}
