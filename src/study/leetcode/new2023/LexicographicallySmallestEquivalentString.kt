9836503772470212f95b7ea4c45e6473e5a28b61
Kangmin <kms0644804@naver.com>
2023-01-15 19:43:47 +0900

    1061. Lexicographically Smallest Equivalent String union find

--- src/study/leetcode/new2023/LexicographicallySmallestEquivalentString.kt
+++ src/study/leetcode/new2023/LexicographicallySmallestEquivalentString.kt
@@ -2,51 +2,45 @@ package study.leetcode.new2023
 
 
 object LexicographicallySmallestEquivalentString {
+
+    // union find로 재풀이
     fun smallestEquivalentString(s1: String, s2: String, baseStr: String): String {
-        val letterGroupMap = HashMap<Char, Char>()  // save a to z
-        val groupLettersMap = HashMap<Char, MutableSet<Char>>()
-        var c = 'a'
-        while (c <= 'z') {
-            letterGroupMap[c] = c
-            val set = HashSet<Char>()
-            set.add(c)
-            groupLettersMap[c] = set // set에 하나만 저장 (a to z까지)
-            c++ // 다음 알파벳
+
+        val unionFind = UnionFind()
+
+        for (i in s1.indices) {
+            val c1 = s1[i]
+            val c2 = s2[i]
+            unionFind.union(c1, c2)
         }
-        val length = s1.length
-        // s1 길이만큼 s2랑 각 철자별로 비교
-        for (i in 0 until length) {
-            val char1 = letterGroupMap[s1[i]]!!
-            val char2 = letterGroupMap[s2[i]]!!
 
-            // 서로 다르면?
-            if (char1 != char2) {
-                val set1 = groupLettersMap[char1]!!
-                val set2 = groupLettersMap[char2]!!
+        val sb = StringBuilder()
+        for (char in baseStr.toCharArray()) {
+            sb.append('a' + unionFind.find(char))
+        }
+        return sb.toString()
+    }
 
-                // 각자의 set 가져 와서 알파벳 순서가 빠른 알파벳을 순서가 느린 알파벳 위치에 바꿔 넣기
-                if (char1 < char2) {
-                    for (item in set2) {
-                        letterGroupMap[item] = char1
-                    }
-                    set1.addAll(set2)
-                    groupLettersMap[char1] = set1
-                    groupLettersMap.remove(char2)
-                } else {
-                    for (item in set1) {
-                        letterGroupMap[item] = char2
-                    }
-                    set2.addAll(set1)
-                    groupLettersMap[char2] = set2
-                    groupLettersMap.remove(char1)
-                }
+    class UnionFind {
+        private val arr = IntArray(26) { i -> i }
+
+        fun union(a: Char, b: Char) {
+            val x = find(a)
+            val y = find(b)
+            if (x < y) {
+                arr[y] = x
+            } else {
+                arr[x] = y
             }
         }
-        val array = baseStr.toCharArray()
-        for (i in array.indices) {
-            val item = array[i]
-            array[i] = letterGroupMap[item]!!   // 실제로 매핑될 알파벳 철자로 바꾸기
+
+        fun find(x: Char): Int {
+            var t = x
+            while (t - 'a' != arr[t - 'a']) {
+                arr[t - 'a'] = arr[arr[t - 'a']]
+                t = (arr[t - 'a'] + 'a'.code).toChar()
+            }
+            return t - 'a'
         }
-        return String(array)
     }
 }
