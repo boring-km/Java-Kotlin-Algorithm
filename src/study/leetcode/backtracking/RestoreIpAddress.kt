98c209e9686022af013a4cf99dfc90ae7aff9be4
Kangmin <kms0644804@naver.com>
2023-01-21 21:21:49 +0900

    93. Restore IP Addresses 주석 추가

--- src/study/leetcode/backtracking/RestoreIpAddress.kt
+++ src/study/leetcode/backtracking/RestoreIpAddress.kt
@@ -6,11 +6,12 @@ import java.util.LinkedList
 object RestoreIpAddress {
 
     val result = mutableListOf<String>()
-    val cur:Deque<String> = LinkedList()
+    val cur: Deque<String> = LinkedList()
     fun restoreIpAddresses(s: String): List<String> {
 
         if (s.length < 4 || s.length > 12) return result
 
+        // 0부터 시작, 남은 점은 4개
         dfs(0, 4, s)
 
         return result
@@ -18,17 +19,28 @@ object RestoreIpAddress {
 
     private fun dfs(i: Int, point: Int, s: String) {
         val n = s.length
+        // 찍을 점이 없다면,
         if (point == 0) {
+            // 마지막 문자열에서 그동안 큐에 담긴 문자열에 . 붙이기
             if (i == n) {
                 result.add(cur.joinToString(separator = "."))
             }
             return
         }
-        for (k in i until Math.min(i + 3, n)) {
-            val str = s.substring(i, k + 1)
-            if (point * 3 < s.substring(k).length || (str[0] == '0' && str.length > 1) || str.toInt() > 255) return
+
+        // 현재 위치부터 다음 점 찍기 전까지, 그리고 n을 넘으면 안됨 (문자열 끝)
+        for (p in i until Math.min(i + 3, n)) {
+            // 현재 위치에서 다음 점 찍기 전까지 남은 갯수만큼 문자열 자르기
+            val str = s.substring(i, p + 1)
+            // 예외 조건: 12개보다 더 길면 x, 시작이 0으로 시작하는 두자리 이상 값 x, 255 넘으면 x
+            if (12 < s.substring(p).length ||
+                (str[0] == '0' && str.length > 1) ||
+                str.toInt() > 255
+            ) return
+
             cur.add(str)
-            dfs(k+1, point-1, s)
+            // 다음 위치, 점 갯수 소모
+            dfs(p + 1, point - 1, s)
             cur.removeLast()
         }
     }
