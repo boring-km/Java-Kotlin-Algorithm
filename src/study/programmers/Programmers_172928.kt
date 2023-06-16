50ebaa2cc22b5cef152ecb31ea43180ba1c5f457
boring-km <kms0644804@naver.com>
2023-06-17 00:21:05 +0900

    프로그래머스 공원 산책 성공

--- src/study/programmers/Programmers_172928.kt
+++ src/study/programmers/Programmers_172928.kt
@@ -1,76 +1,50 @@
 package study.programmers
 
-// 실패: 정답률 75%
 object Programmers_172928 {
     fun solution(park: Array<String>, routes: Array<String>): IntArray {
-        var answer: IntArray = intArrayOf()
+        var x = 0; var y = 0
+        val h = park.size
+        val w = park[0].length
 
         // E, S, W, N
         val move = arrayOf(intArrayOf(0, 1), intArrayOf(1, 0), intArrayOf(0, -1), intArrayOf(-1, 0))
+        val map = HashMap<String, IntArray>()
+        map["E"] = move[0]
+        map["S"] = move[1]
+        map["W"] = move[2]
+        map["N"] = move[3]
 
-        var sy = 0
-        var sx = 0
-        var ySize = park.size
-        var xSize = park[0].length
-
-        for (y in 0 until ySize) {
-            for (x in 0 until xSize) {
-                if (park[y][x] == 'S') {
-                    sy = y
-                    sx = x
+        for (i in 0 until h) {
+            for (j in 0 until w) {
+                if (park[i][j] == 'S') {
+                    y = i; x = j
                     break
                 }
             }
         }
 
         for (route in routes) {
-            val t = route.split(" ")
-            val direction = t[0]
-            val dist = t[1]
-            val arr = movement(direction, move)
+            val array = route.split(" ")
+            val direction = array[0]
+            val dist = array[1].toInt()
+            val dy = y
+            val dx = x
 
-            val yDist = arr[0] * dist.toInt()
-            val xDist = arr[1] * dist.toInt()
-            val ty = sy + yDist
-            val tx = sx + xDist
+            for (i in 0 until dist) {
+                val ty = y + map[direction]!![0]
+                val tx = x + map[direction]!![1]
 
-            if (ty in 0 until ySize && tx in 0 until xSize) {
-                var check = true
-                // x
-                if (xDist != 0) {
-                    for (i in 1..xDist) {
-                        if (park[sy][sx + i] != 'O') {
-                            check = false
-                            break
-                        }
-                    }
-                }
-                // y
-                if (yDist != 0) {
-                    for (i in 1..yDist) {
-                        if (park[sy + i][sx] != 'O') {
-                            check = false
-                            break
-                        }
-                    }
-                }
-                if (check) {
-                    sy = ty
-                    sx = tx
+                if (tx in 0 until w && ty in 0 until h && (park[ty][tx] != 'X')) {
+                    x = tx
+                    y = ty
+                } else {
+                    x = dx
+                    y = dy
+                    break
                 }
             }
         }
 
-        return intArrayOf(sy, sx)
-    }
-
-    fun movement(text: String, move: Array<IntArray>): IntArray {
-        return when(text) {
-            "E" -> move[0]
-            "S" -> move[1]
-            "W" -> move[2]
-            "N" -> move[3]
-            else -> throw Exception()
-        }
+        return intArrayOf(y, x)
     }
 }
